577b4e5f63a5e3045d72c8c8caaa13a59c4ecafa
JT Turner <j.tturner14@yahoo.com>
2020-03-15 19:59:02 -0400

    started contact me page

---
+++ reactportfolio/src/pages/Contact/contact.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function Contact() {
+    return (
+        <div className="container-fluid-main">
+            <div className="row">
+                <div className="col-10">
+                    <h2>Contact Me</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
