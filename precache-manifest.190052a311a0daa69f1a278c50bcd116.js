58fff9b8e337e0635473f1f8487b7a63f8aff600
JT Turner <j.tturner14@yahoo.com>
2020-03-21 09:39:44 -0400

    Updates

---
+++ precache-manifest.190052a311a0daa69f1a278c50bcd116.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ba891387f0763ad0d633c658dc4141",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "231b68d8d0b00a5ee77f",
+    "url": "/reactportfolio/static/css/main.8f90b31c.chunk.css"
+  },
+  {
+    "revision": "c6750d9c3415b04af4ed",
+    "url": "/reactportfolio/static/js/2.5d960e32.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactportfolio/static/js/2.5d960e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231b68d8d0b00a5ee77f",
+    "url": "/reactportfolio/static/js/main.6a5d45b3.chunk.js"
+  },
+  {
+    "revision": "4ab9923d9807f9b227e1",
+    "url": "/reactportfolio/static/js/runtime-main.254f27b7.js"
+  },
+  {
+    "revision": "989495691d8f2c50fb776420814865e8",
+    "url": "/reactportfolio/static/media/CodeQuiz.98949569.png"
+  },
+  {
+    "revision": "65941225a2384855241df22438e585dd",
+    "url": "/reactportfolio/static/media/DayPlanner.65941225.png"
+  },
+  {
+    "revision": "7a201cf2fb7b344c651d9efa65b7973c",
+    "url": "/reactportfolio/static/media/Giftify.7a201cf2.png"
+  },
+  {
+    "revision": "31e6e92b048884df0776e776fb80a359",
+    "url": "/reactportfolio/static/media/IntuitiveBanana.31e6e92b.png"
+  },
+  {
+    "revision": "79cc35213bcb613b8e5dbc27cd7ed044",
+    "url": "/reactportfolio/static/media/Javier_Turner_Resume.79cc3521.pdf"
+  },
+  {
+    "revision": "94b0d35da15095ddca9574731b117f10",
+    "url": "/reactportfolio/static/media/NoteTaker.94b0d35d.png"
+  },
+  {
+    "revision": "bd0ccdb9e78175a1833895e243b39005",
+    "url": "/reactportfolio/static/media/PasswordGenerator.bd0ccdb9.png"
+  },
+  {
+    "revision": "31f778bcdf8227c005817bfb97de4be2",
+    "url": "/reactportfolio/static/media/profilepicture.31f778bc.jpg"
+  }
+]);
